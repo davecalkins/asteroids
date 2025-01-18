61ee9da7b431ecc166d080aadb1b36fd61a5d70e
davecalkins <2372599+davecalkins@users.noreply.github.com>
2025-01-18 13:19:13 -0500

    limit frame rate

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from constants import *
 def main():
     pygame.init()
     pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -15,6 +18,8 @@ def main():
                 return
 
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
