9e9c4e41700adff85ccf6906282e136f0016cfdd
davecalkins <2372599+davecalkins@users.noreply.github.com>
2025-01-18 18:30:54 -0500

    added player ship

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
@@ -11,11 +12,17 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 )
+
+    surf = pygame.display.get_surface()
+
     while True:
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        player.draw(surf)
 
         pygame.display.flip()
 
